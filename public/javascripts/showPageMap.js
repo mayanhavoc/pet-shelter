e9f0344d208b5b59930c4e08c2e1af376da6d908
Roberto <mayanhavoc@gmail.com>
2022-01-31 15:32:30 -0800

    tries to fix maps bug

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = mapToken;
+mapboxgl.accessToken = mapBoxToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
